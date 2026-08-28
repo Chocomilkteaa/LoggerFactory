937659b9055224d96e01c846dd328e2d689cb4e1
ChocoMilktea <milktea126421@gmail.com>
2026-08-28 02:04:34 +0000

    feat: update vitest config file for browser test env support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from "vitest/config";
+import { playwright } from '@vitest/browser-playwright'
 
 const config = defineConfig({
   test: {
-    include: ["tests/*.test.ts"],
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "node",
+          environment: "node",
+          include: ["tests/*.node.test.ts"],
+        }
+      },
+      {
+        extends: true,
+        test: {
+          name: "browser",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [
+              {
+                browser: "chromium",
+                headless: true,
+              }
+            ],
+          },
+          include: ["tests/*.browser.test.ts"],
+        }
+      }
+    ],
   },
 });
 
