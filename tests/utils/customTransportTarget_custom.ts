d3a89378a633d3ca1747beb36334f4e432a4a62b
ChocoMilktea <milktea126421@gmail.com>
2026-08-31 08:54:04 +0000

    feat: add custom transport target for logging events

---
+++ tests/utils/customTransportTarget_custom.ts
@@ -0,0 +1,54 @@
+import { appendFile } from "fs/promises";
+
+import { createCustomLoggerTransportTarget } from "../../src/node/createCustomLoggerTransportTarget.ts";
+
+interface TestTransportOptions {
+    destination: string;
+}
+
+async function appendEvent(
+    destination: string,
+    event: unknown,
+) {
+    await appendFile(
+        destination,
+        `${JSON.stringify(event)}\n`,
+    );
+}
+
+function target(options: TestTransportOptions) {
+    return createCustomLoggerTransportTarget({
+        close: async () => {
+            await appendEvent(options.destination, {
+                type: "close",
+            });
+        },
+        flush: async () => {
+            await appendEvent(options.destination, {
+                type: "flush",
+            });
+        },
+        onError: async (error) => {
+            await appendEvent(options.destination, {
+                message: error instanceof Error
+                    ? error.message
+                    : String(error),
+                type: "error",
+            });
+        },
+        options,
+        parse: (input) => {
+            return {
+                message: (input as { msg: string }).msg
+            };
+        },
+        write: async (value) => {
+            await appendEvent(options.destination, {
+                type: "write",
+                value,
+            });
+        },
+    });
+}
+
+export default target;
