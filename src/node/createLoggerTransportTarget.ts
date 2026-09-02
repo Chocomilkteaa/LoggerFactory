548f4e6de916d3fea64ccb4d1fad7758c816294d
ChocoMilktea <milktea126421@gmail.com>
2026-09-02 07:53:44 +0000

    fix: add level override to pretty transport

--- src/node/createLoggerTransportTarget.ts
+++ src/node/createLoggerTransportTarget.ts
@@ -4,33 +4,33 @@ type LogLevel = pino.LevelWithSilent;
 
 type LoggerTransportTarget =
   | {
-      levelOverride?: LogLevel;
-      options: {
-        append: boolean;
-        destination: string;
-        mkdir: boolean;
-        sync?: boolean;
-      };
-      type: "file";
-    }
-  | {
-      levelOverride?: LogLevel;
-      options: {
-        destination: string;
-      };
-      target: string;
-      type: "custom";
-    }
-  | {
-      levelOverride?: LogLevel;
+    levelOverride?: LogLevel;
+    options: {
+      append: boolean;
+      destination: string;
+      mkdir: boolean;
       sync?: boolean;
-      type: "pretty";
-    }
-  | {
-      levelOverride?: LogLevel;
-      sync?: boolean;
-      type: "stdout";
     };
+    type: "file";
+  }
+  | {
+    levelOverride?: LogLevel;
+    options: {
+      destination: string;
+    };
+    target: string;
+    type: "custom";
+  }
+  | {
+    levelOverride?: LogLevel;
+    sync?: boolean;
+    type: "pretty";
+  }
+  | {
+    levelOverride?: LogLevel;
+    sync?: boolean;
+    type: "stdout";
+  };
 
 const STDOUT_FILE_DESCRIPTOR = 1;
 
@@ -62,6 +62,7 @@ function createLoggerTransportTarget(target: LoggerTransportTarget): pino.Transp
       };
     case "pretty":
       return {
+        level: target.levelOverride,
         options: {
           colorize: true,
           destination: STDOUT_FILE_DESCRIPTOR,
